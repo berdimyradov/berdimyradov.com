42e44fb83b15d16e506d3ec870276fa49941634f
berdimyradov <kberdimyradov@gmail.com>
2023-05-01 17:28:17 +0300

    :poop: Reverting

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -2,9 +2,8 @@ import { TBody, TD, TH, THead, TR, Table } from "@/components/Table";
 import { apiFetch } from "@/lib/api";
 
 export default async function History() {
-  // const response = await apiFetch("/history", { cache: "no-cache" });
-  // const { payments } = await response.json();
-  const payments = [];
+  const response = await apiFetch("/history", { cache: "no-cache" });
+  const { payments } = await response.json();
 
   return (
     <div className="mt-14 w-full flex justify-center items-center">
