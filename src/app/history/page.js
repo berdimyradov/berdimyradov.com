a6a30f9cf2582d7ded19bf43622211032cf85498
berdimyradov <kberdimyradov@gmail.com>
2023-05-01 17:26:22 +0300

    :poop: Revert after deploy

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -2,8 +2,9 @@ import { TBody, TD, TH, THead, TR, Table } from "@/components/Table";
 import { apiFetch } from "@/lib/api";
 
 export default async function History() {
-  const response = await apiFetch("/history", { cache: "no-cache" });
-  const { payments } = await response.json();
+  // const response = await apiFetch("/history", { cache: "no-cache" });
+  // const { payments } = await response.json();
+  const payments = [];
 
   return (
     <div className="mt-14 w-full flex justify-center items-center">
