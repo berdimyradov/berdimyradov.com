bdb3448db30c2fbe67ab63d76a040232b1dd2244
berdimyradov <kberdimyradov@gmail.com>
2023-08-07 19:48:38 +0300

    :speech_balloon: Update text for skills page

---
+++ src/entities/skills/model/data.js
@@ -0,0 +1,159 @@
+export const SKILLS = [
+  {
+    section: "Mobile Technologies",
+    tools: [
+      {
+        tool: "React Native",
+        desc: "A popular framework for building native mobile apps using JavaScript and React.",
+      },
+      {
+        tool: "React Navigation",
+        desc: "A routing and navigation library for React Native applications.",
+      },
+      {
+        tool: "App Store Connect",
+        desc: "Apple's platform for managing and distributing iOS applications.",
+      },
+      {
+        tool: "Google Play Console",
+        desc: "Google's platform for managing and distributing Android applications.",
+      },
+      {
+        tool: "Firebase",
+        desc: "A comprehensive app development platform by Google, with analytics, databases, messaging, and more.",
+      },
+    ],
+  },
+  {
+    section: "Web Technologies",
+    tools: [
+      {
+        tool: "TypeScript",
+        desc: "A superset of JavaScript adding static typing and other features.",
+      },
+      {
+        tool: "React",
+        desc: "A JavaScript library for building user interfaces.",
+      },
+      {
+        tool: "Redux",
+        desc: "A predictable state container for JavaScript applications.",
+      },
+      {
+        tool: "Node.js",
+        desc: "A JavaScript runtime for building scalable network applications.",
+      },
+      {
+        tool: "WebSocket",
+        desc: "A protocol providing full-duplex communication channels over a single TCP connection.",
+      },
+      {
+        tool: "NPM",
+        desc: "A package manager for JavaScript, to manage modules and dependencies.",
+      },
+      {
+        tool: "Yarn",
+        desc: "A fast, reliable, and secure package manager for JavaScript.",
+      },
+      {
+        tool: "Webpack",
+        desc: "A module bundler used to bundle JavaScript files and additional resources.",
+      },
+    ],
+  },
+  {
+    section: "Version Control Systems",
+    tools: [
+      {
+        tool: "Git",
+        desc: "A distributed version control system for tracking changes in source code.",
+      },
+      {
+        tool: "SVN",
+        desc: "Apache Subversion, a centralized version control system.",
+      },
+      {
+        tool: "GitFlow",
+        desc: "A branching model for Git, supporting the parallel development of features.",
+      },
+      {
+        tool: "TrunkBased",
+        desc: "A source-control branching model, where developers work in short-lived branches or directly in trunk.",
+      },
+    ],
+  },
+  {
+    section: "DBMS",
+    tools: [
+      {
+        tool: "MySQL",
+        desc: "An open-source relational database management system.",
+      },
+      {
+        tool: "MongoDB",
+        desc: "A document-based distributed database designed for modern application development.",
+      },
+      {
+        tool: "DataGrip",
+        desc: "A professional database management tool by JetBrains.",
+      },
+    ],
+  },
+  {
+    section: "IDEs & Editors",
+    tools: [
+      {
+        tool: "WebStorm",
+        desc: "A professional JavaScript IDE by JetBrains, with support for modern frameworks.",
+      },
+      {
+        tool: "Visual Studio Code",
+        desc: "A free source-code editor by Microsoft with support for debugging, syntax highlighting, and much more.",
+      },
+      {
+        tool: "NeoVim",
+        desc: "An extension of Vim, aimed at improving extensibility and adding new features for efficient code editing",
+      },
+    ],
+  },
+  {
+    section: "Software development methodologies",
+    tools: [
+      {
+        tool: "Jira",
+        desc: "An agile project management tool supporting any agile methodology.",
+      },
+      {
+        tool: "Confluence",
+        desc: "A collaboration tool for team collaboration and documentation.",
+      },
+      { tool: "Trello", desc: "A Kanban-style project management app." },
+      {
+        tool: "Waterfall",
+        desc: "A traditional sequential project management methodology.",
+      },
+      {
+        tool: "Agile",
+        desc: "A methodology emphasizing iterative progress and collaboration.",
+      },
+      { tool: "Kanban", desc: "A visual workflow management method." },
+      {
+        tool: "Scrum",
+        desc: "An agile methodology for incremental and iterative project work.",
+      },
+    ],
+  },
+  {
+    section: "Other tools",
+    tools: [
+      {
+        tool: "Slack",
+        desc: "A collaboration hub for teamwork, with channels, private groups, and direct messaging.",
+      },
+      {
+        tool: "Figma",
+        desc: "A cloud-based design tool for collaborative interface design.",
+      },
+    ],
+  },
+];
