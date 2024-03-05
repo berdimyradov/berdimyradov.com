da5673091105e98fa795ede32738c7a6b3ea9d33
Kerim Berdimyradov <kberdimyradov@gmail.com>
2024-03-05 11:38:23 +0300

    :sparkles: Add service-worker for pushy

---
+++ public/service-worker.js
@@ -0,0 +1,1 @@
+importScripts("https://sdk.pushy.me/web/1.0.17/pushy-service-worker.js");
--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Container } from "@/shared/components/Container";
+import avatarImage from "@/shared/images/avatar.jpg";
+import { Popover, Transition } from "@headlessui/react";
+import clsx from "clsx";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useTheme } from "next-themes";
-import { Popover, Transition } from "@headlessui/react";
-import clsx from "clsx";
-
-import { Container } from "@/shared/components/Container";
-import avatarImage from "@/shared/images/avatar.jpg";
+import { Fragment, useEffect, useRef, useState } from "react";
 
 function CloseIcon(props) {
   return (
@@ -48,8 +47,7 @@ function SunIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
       aria-hidden="true"
-      {...props}
-    >
+      {...props}>
       <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
       <path
         d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
@@ -97,8 +95,7 @@ function MobileNavigation(props) {
           enterTo="opacity-100"
           leave="duration-150 ease-in"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
+          leaveTo="opacity-0">
           <Popover.Overlay className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
         </Transition.Child>
         <Transition.Child
@@ -108,12 +105,10 @@ function MobileNavigation(props) {
           enterTo="opacity-100 scale-100"
           leave="duration-150 ease-in"
           leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
+          leaveTo="opacity-0 scale-95">
           <Popover.Panel
             focus
-            className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
-          >
+            className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800">
             <div className="flex flex-row-reverse items-center justify-between">
               <Popover.Button aria-label="Close menu" className="-m-1 p-1">
                 <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
@@ -148,9 +143,8 @@ function NavItem({ href, children }) {
           "relative block px-3 py-2 transition",
           isActive
             ? "text-teal-500 dark:text-teal-400"
-            : "hover:text-teal-500 dark:hover:text-teal-400"
-        )}
-      >
+            : "hover:text-teal-500 dark:hover:text-teal-400",
+        )}>
         {children}
         {isActive && (
           <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
@@ -177,18 +171,51 @@ function ThemeToggle() {
   let { resolvedTheme, setTheme } = useTheme();
   let otherTheme = resolvedTheme === "dark" ? "light" : "dark";
   let [mounted, setMounted] = useState(false);
+  let [isRegistered, setIsRegistered] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  const onThemeToogle = async () => {
+    // Register visitor's browser for push notifications
+    const Pushy = (await import("pushy-sdk-web")).default;
+    if (Pushy && Pushy.register && !isRegistered) {
+      Pushy.register({
+        appId: "65e6ea5b65c1588976608a0e",
+      })
+        .then(function (deviceToken) {
+          console.log("Pushy device token: " + deviceToken);
+          setIsRegistered(true);
+        })
+        .catch(function (err) {
+          alert("Registration failed: " + err.message);
+        });
+    } else {
+      console.warn("Pushy SDK is not imported");
+    }
+
+    // Handle push notifications (only when web page is open)
+    Pushy.setNotificationListener(function (data) {
+      // Print notification payload data
+      console.log("Received notification: " + JSON.stringify(data));
+
+      // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+      let message = data.message || "Test notification";
+
+      // Display an alert with message sent from server
+      alert("Received notification: " + message);
+    });
+
+    setTheme(otherTheme);
+  };
 
   return (
     <button
       type="button"
       aria-label={mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"}
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
-      onClick={() => setTheme(otherTheme)}
-    >
+      onClick={onThemeToogle}>
       <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
       <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
     </button>
@@ -206,7 +233,7 @@ function AvatarContainer({ className, ...props }) {
     <div
       className={clsx(
         className,
-        "h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10"
+        "h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10",
       )}
       {...props}
     />
@@ -219,15 +246,14 @@ function Avatar({ large = false, className, ...props }) {
       href="/"
       aria-label="Home"
       className={clsx(className, "pointer-events-auto")}
-      {...props}
-    >
+      {...props}>
       <Image
         src={avatarImage}
         alt=""
         sizes={large ? "4rem" : "2.25rem"}
         className={clsx(
           "rounded-full bg-zinc-100 object-cover dark:bg-zinc-800",
-          large ? "h-16 w-16" : "h-9 w-9"
+          large ? "h-16 w-16" : "h-9 w-9",
         )}
         priority
       />
@@ -241,6 +267,22 @@ export function Header() {
   let headerRef = useRef();
   let avatarRef = useRef();
   let isInitial = useRef(true);
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js", { scope: "/" })
+        .then((registration) => {
+          console.log(
+            "Service worker registered successfully. Scope:",
+            registration.scope,
+          );
+        })
+        .catch((error) => {
+          console.error("Service worker registration failed:", error);
+        });
+    }
+  }, []);
 
   useEffect(() => {
     let downDelay = avatarRef.current?.offsetTop ?? 0;
@@ -259,7 +301,7 @@ export function Header() {
       let scrollY = clamp(
         window.scrollY,
         0,
-        document.body.scrollHeight - window.innerHeight
+        document.body.scrollHeight - window.innerHeight,
       );
 
       if (isInitial.current) {
@@ -311,7 +353,7 @@ export function Header() {
 
       setProperty(
         "--avatar-image-transform",
-        `translate3d(${x}rem, 0, 0) scale(${scale})`
+        `translate3d(${x}rem, 0, 0) scale(${scale})`,
       );
 
       let borderScale = 1 / (toScale / scale);
@@ -345,8 +387,7 @@ export function Header() {
         style={{
           height: "var(--header-height)",
           marginBottom: "var(--header-mb)",
-        }}
-      >
+        }}>
         {isHomePage && (
           <>
             <div
@@ -355,12 +396,10 @@ export function Header() {
             />
             <Container
               className="top-0 order-last -mb-3 pt-3"
-              style={{ position: "var(--header-position)" }}
-            >
+              style={{ position: "var(--header-position)" }}>
               <div
                 className="top-[var(--avatar-top,theme(spacing.3))] w-full"
-                style={{ position: "var(--header-inner-position)" }}
-              >
+                style={{ position: "var(--header-inner-position)" }}>
                 <div className="relative">
                   <AvatarContainer
                     className="absolute left-0 top-3 origin-left transition-opacity"
@@ -382,12 +421,10 @@ export function Header() {
         <div
           ref={headerRef}
           className="top-0 z-10 h-16 pt-6"
-          style={{ position: "var(--header-position)" }}
-        >
+          style={{ position: "var(--header-position)" }}>
           <Container
             className="top-[var(--header-top,theme(spacing.6))] w-full"
-            style={{ position: "var(--header-inner-position)" }}
-          >
+            style={{ position: "var(--header-inner-position)" }}>
             <div className="relative flex gap-4">
               <div className="flex flex-1">
                 {!isHomePage && (
