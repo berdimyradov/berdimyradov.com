2061a7a051bc0a3192ffc7a5b31026511a8839ce
Kerim Berdimyradov <kberdimyradov@gmail.com>
2025-09-19 01:37:25 +0500

    :bug: Revert /cv

--- src/app/cv/page.jsx
+++ src/app/cv/page.jsx
@@ -1,31 +1,37 @@
-import "./cv.css";
-import { Dancing_Script } from "next/font/google";
+// import "./cv.css";
+// import { Dancing_Script } from "next/font/google";
 
-const font = Dancing_Script({
-  subsets: ["latin"],
-});
+// const font = Dancing_Script({
+//   subsets: ["latin"],
+// });
+
+// export default async function CV({ params }) {
+//   return (
+//     <div className="cv-container">
+//       <div className="paper">
+//         <section>
+//           <header>
+//             <h1 className={font.className}>KERIM BERDIMYRADOV</h1>
+
+//             <p>From TUESDAY, APRIL 27, to THURSDAY, APRIL 29, 1784.</p>
+//           </header>
+
+//           <main>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+//             ex dicta, laudantium est porro maxime ratione animi? Totam itaque
+//             minima sequi! Dolore quibusdam vitae exercitationem facilis
+//             inventore, vero recusandae neque.
+//           </main>
+
+//           <footer>Footer</footer>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { redirect } from "next/navigation";
 
 export default async function CV({ params }) {
-  return (
-    <div className="cv-container">
-      <div className="paper">
-        <section>
-          <header>
-            <h1 className={font.className}>KERIM BERDIMYRADOV</h1>
-
-            <p>From TUESDAY, APRIL 27, to THURSDAY, APRIL 29, 1784.</p>
-          </header>
-
-          <main>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-            ex dicta, laudantium est porro maxime ratione animi? Totam itaque
-            minima sequi! Dolore quibusdam vitae exercitationem facilis
-            inventore, vero recusandae neque.
-          </main>
-
-          <footer>Footer</footer>
-        </section>
-      </div>
-    </div>
-  );
+  redirect("/CV_Berdimyradov-Kerim[FE].pdf");
 }
