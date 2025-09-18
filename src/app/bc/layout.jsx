414a42446bacb720d1413f7a67e6e804c138c56e
Kerim Berdimyradov <kberdimyradov@gmail.com>
2025-09-19 00:57:03 +0500

    :sparkles: Add new /bc page

---
+++ src/app/bc/layout.jsx
@@ -0,0 +1,5 @@
+import { Layout } from "@/shared/components/Layout";
+
+export default function RootLayout({ children }) {
+  return <Layout>{children}</Layout>;
+}
---
+++ src/app/bc/page.jsx
@@ -0,0 +1,7 @@
+import BusinessCardPage from "@/screens/BusinessCard";
+
+export const metadata = {};
+
+export default function () {
+  return <BusinessCardPage />;
+}
